09ae3fdb235977e24b24e119d115f6b0d0aac292
talitamsx <talitamsx@gmail.com>
2023-01-17 12:58:35 -0300

    estrutura html e css

--- src/cipher.js
+++ src/cipher.js
@@ -2,4 +2,8 @@ const cipher = {
   // ...
 };
 
+const mensagemCodificada = () => {
+  const codifica = document.usuario.elements["usuario"].value
+}
+
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+//p1 é uma variavel criada para poder mostrar a info na tela, atraves do getElementysByTagName eu acesso a tag p, o 0 dentro de colchetes esta dizendo que se trata do primeiro paragrafo <p>
+
